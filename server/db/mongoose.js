5525b1115b07ddb8e1a54f1e80b809f04b0aaf6d
Himanshu Santolia <himanshusantolia@live.com>
2018-05-12 00:32:10 +0530

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise ; // telling mongoose to use promises
-mongoose.connect('mongodb://localhost:27017/TodoApp') ;
+mongoose.connect(process.env.DATABASE_URL ||'mongodb://localhost:27017/TodoApp') ;
 
 module.exports = { mongoose};
+
+// process.env.DATABASE_URL - is the url of mongodb hosted at mlab
+// it is defined in heroku environment
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo') ;
 var {User} = require('./models/user') ;
 
 var app = express();
+const port = process.env.PORT || 3000 ;
 
 //middleware
 app.use(bodyParser.json());
@@ -57,10 +58,10 @@ app.get('/todos/:id',(req,res)=>{
 
 
 
-// ----------------------------------------------------- //
+// --------------------------------------------------------------- //
 
-app.listen(3000,()=>{
-  console.log('Started on port 3000');
+app.listen(port,()=>{
+  console.log('App is live on',port);
 });
 
 
