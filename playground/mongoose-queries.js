0b9fb280fc417c7be633a24a56cd706f2ec1fb12
Himanshu Santolia <himanshusantolia@live.com>
2018-05-11 19:14:28 +0530

    Add Queries playground file

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -4,7 +4,7 @@ const {mongoose} = require('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user');
 
-// var id = '5af54d64c19c552bfcf82c5d' ;
+var id = '5af54d64c19c552bfcf82c5d' ;
 // var id = '6af54d64c19c552bfcf82c5dsadsad' ;   // madeup id
 
 
@@ -12,31 +12,30 @@ const {User} = require('./../server/models/user');
 //   console.log('ID not valid') ;
 // }
 
-
-//
-// Todo.find({
-//   _id : id
-//   //mongoose is autmatically going to convert this string into object ID
-// }).then((todos)=>{
-//   if(todos.length==0) return console.log('ID not found') ;
-//   console.log('Todos',todos);
-// }) ;
-
-// Todo.findOne({
-//   _id : id
-// }).then((todo)=>{
-//   if(!todo) return console.log('ID not found') ;
-//   console.log('Todo',todo);
-// });
-
-// Todo.findById(id).then((todo)=>{
-//   if(!todo) return console.log('ID not found') ;
-//   console.log('Todo by Id',todo);
-// }).catch((err)=>console.log(err));
-
-var id = '5af4c43b86319f0c984f47e9';
-User.findById({
+// below functions will throw error if ID is invalid
+Todo.find({
   _id : id
+  //mongoose is autmatically going to convert this string into object ID
+}).then((todos)=>{
+  if(todos.length==0) return console.log('ID not found') ;
+  console.log('Todos',todos);
+}) ;
+
+Todo.findOne({
+  _id : id
+}).then((todo)=>{
+  if(!todo) return console.log('ID not found') ;
+  console.log('Todo',todo);
+});
+
+Todo.findById(id).then((todo)=>{
+  if(!todo) return console.log('ID not found') ;
+  console.log('Todo by Id',todo);
+}).catch((err)=>console.log(err));
+
+var id_user = '5af4c43b86319f0c984f47e9';
+User.findById({
+  _id : id_user
 }).then((user)=>{
   if(!user) return console.log('Id not found');
 
