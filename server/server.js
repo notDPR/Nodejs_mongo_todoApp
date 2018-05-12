ef0a5f06c624458aab865927295e11942821e5af
Himanshu Santolia <himanshusantolia@live.com>
2018-05-12 15:24:05 +0530

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -65,6 +66,38 @@ app.delete('/todos/:id' , (req,res)=>{
   .catch((err)=>{
     res.status(400).send({});
   });
+});
+
+app.patch('/todos/:id' , (req,res)=>{
+  var id = req.params.id ;
+  var body = _.pick(req.body , ['text','completed']);
+  if(!ObjectID.isValid(id)) return res.status(400).send();
+
+  if(_.isBoolean(body.completed) && body.completed)
+  {
+    body.completedAt =  new Date().getTime() ;
+    // gets no. of milliseconds from 1/1/1970
+  }
+  else{
+    body.completedAt = false ;
+    body.completed = null ;
+  }
+
+  Todo.findByIdAndUpdate(id,{
+    $set : body
+  },
+  {new : true} // returns updated todo
+  )
+  .then((todo)=>{
+    if(!todo) return res.status(404).send();
+
+    res.send({todo}) ;
+  })
+  .catch((err)=>res.status(400).send()) ;
+
+
+
+
 });
 
 
